0fdeb83c8b12ff57be4555a6e069b2fe7ef5b214
hackritik000 <hackritik000@gmail.com>
2025-06-03 16:09:48 +0530

    adding codium and s

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- First, remove default Flash mapping
+vim.keymap.del("n", "s")
+vim.keymap.del("x", "s")
+
+-- Then, assign Flash to another key
+vim.keymap.set("n", "<leader>s", function()
+  require("flash").jump()
+end, { desc = "Flash jump" })
