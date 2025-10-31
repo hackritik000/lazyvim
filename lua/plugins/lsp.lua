eb3a4d1b1ac4c48115aed947e74d642d4d5a5198
hackritik000 <hackritik000@gmail.com>
2025-10-31 15:41:47 +0530

    add lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,13 +5,27 @@ return {
       servers = {
         vtsls = { -- Replace tsserver with vtsls
           root_dir = function(fname)
-            return require("lspconfig.util").root_pattern("package.json", ".git")(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern("tsconfig.json", "package.json", ".git")(fname)
           end,
           settings = {
             typescript = {
               tsserver = {
                 maxTsServerMemory = 8192, -- Increase memory limit
+                -- Enable incremental compilation for faster analysis
+                incremental = true,
+                -- Disable expensive features if not needed
+                diagnostics = { enable = false, include = { "*.ts", "*.tsx" } },
+                -- Enable workspace folders
+                workspaceFolders = {
+                  supported = true,
+                  changeNotifications = true,
+                },
               },
+              completions = {
+                completeFunctionCalls = false, -- Disable if you don’t need function call completions
+              },
+              workspaceSymbols = { scope = "project" },
               inlayHints = {
                 parameterNames = { enabled = "none" },
                 parameterTypes = { enabled = false },
@@ -25,7 +39,8 @@ return {
         },
         biome = {
           root_dir = function(fname)
-            return require("lspconfig.util").root_pattern("package.json", "biome.json", ".git")(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern("biome.json", "package.json", ".git")(fname)
           end,
         },
       },
